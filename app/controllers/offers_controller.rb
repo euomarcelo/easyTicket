23a3e563824a9a49c4c92f4908c798ddc2827b68
Marcelo Vasques <msouvas@gmail.com>
2015-06-10 20:06:18 -0300

    New page for creting offers, css of menu bar adjusted

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,7 +1,11 @@
 class OffersController < ApplicationController
+  respond_to :html
+
   def new
+    @offer = Offer.new
+    respond_with(@offer)
   end
-  
+
   def search
     @offers = Offer.search params[:search]
   end
