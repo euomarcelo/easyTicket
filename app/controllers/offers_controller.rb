cc5583e0a055607e00428058df8582301d30b099
Marcelo E. Cardoso <marc.cardo@gmail.com>
2015-06-17 22:34:00 -0300

    Não deixa usuário comprar ingresso sem dinheiro. (propagar mudança para auction; colocar flash notice

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -39,9 +39,16 @@ class OffersController < ApplicationController
   end
 
   def buy
-    @offer.update_attribute(:is_active, false)
-    current_user.decrement(:balance, @offer.actual_price)
-    current_user.save
+    current_user.balance -= @offer.actual_price
+    @offer.is_active = false
+    begin 
+      ActiveRecord::Base.transaction do      
+        current_user.save!
+        @offer.save!
+      end
+    rescue
+      redirect_to offers_path, alert: "Saldo insuficiente"
+    end
   end
 
   def approve
